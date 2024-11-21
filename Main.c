667c3087d17cf8d3e13a8e86a452254ab202341e
B1329046 <sunnyhsieh0827@gmail.com>
2024-11-21 14:47:12 +0800

    完成EX1程式碼

---
+++ Main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 使用 5 個 printf 印出 5 行星號
+    printf(”*****\n“);
+    printf(”*****\n“);
+    printf(”*****\n“);
+    printf(”*****\n“);
+    printf(”*****\n“);
+    return 0;
+}
